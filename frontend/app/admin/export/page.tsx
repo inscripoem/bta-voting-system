45feeb228a4fc45e09442dbbefe3e89edeef2dd7
liudan1270 <2731836162@qq.com>
2026-03-06 21:00:54 +0800

    fix: 删除学校管理表单大边框，调整各个管理表单的总体位置，提高UI一致性

--- frontend/app/admin/export/page.tsx
+++ frontend/app/admin/export/page.tsx
@@ -34,7 +34,7 @@ export default function AdminExportPage() {
   }
 
   return (
-    <div className="space-y-6">
+    <div className="py-6 space-y-6">
       <Card>
         <CardHeader>
           <CardTitle>导出投票数据</CardTitle>
